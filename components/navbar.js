b7edac873a348dfb1dfd4baaf8794d70897e1417
wxmansam <wxmansam@gmail.com>
2026-04-24 20:09:05 -0500

    Refactor navbar links and add dropdown menu

--- components/navbar.js
+++ components/navbar.js
@@ -3,18 +3,27 @@ class Navbar extends HTMLElement {
     super();
   }
 
-  connectedCallback() {
+ connectedCallback() {
     this.innerHTML = `
     <nav>
       <ul>
-        <li><a href="./index.html">Home</a></li>
-        <li><a href="../pages/page2.html">Page 2</a></li>
-        <li><a href="../pages/page3.html">Page 3</a></li>
-        <li><a href="../pages/page4.html">Page 4</a></li>
-        <li><a href="../pages/page5.html">Page 5</a></li>
+        <li><a href="./index.html" class="active">HOME</a></li>
+        <li><a href="../pages/page2.html">PAGE 2</a></li>
+        
+        <li class="dropdown">
+          <a href="../pages/page3.html">PAGE 3 ▾</a>
+          <ul class="dropdown-menu">
+            <li><a href="../pages/page3-sub1.html">CRYPTID LIST</a></li>
+            <li><a href="../pages/page3-sub2.html">SIGHTINGS MAP</a></li>
+            <li><a href="../pages/page3-sub3.html">SUBMIT EVIDENCE</a></li>
+          </ul>
+        </li>
+        
+        <li><a href="../pages/page4.html">PAGE 4</a></li>
+        <li><a href="../pages/page5.html">PAGE 5</a></li>
       </ul>
     </nav>`;
   }
-}
+  }
 
 customElements.define("navbar-component", Navbar);
