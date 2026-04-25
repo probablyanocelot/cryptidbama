05f3afcddcc7409deca03e14ad56ba002be7bc51
wxmansam <wxmansam@gmail.com>
2026-04-24 21:24:17 -0500

    Refactor navbar to use dynamic path prefixes

--- components/navbar.js
+++ components/navbar.js
@@ -3,24 +3,29 @@ class Navbar extends HTMLElement {
     super();
   }
 
- connectedCallback() {
-  this.innerHTML = `
-<nav>
-  <ul>
-    <li><a href="/index.html" class="active">HOME</a></li>
-    <li><a href="/pages/cryptidfaq.html">What's A Cryptid?</a></li>
-    <li class="dropdown">
-        <a href="/pages/famouscryptids.html">Famous Cryptids ▾</a>
-        <ul class="dropdown-menu">
-            <li><a href="/pages/ufo.html">UFO</a></li>
-            <li><a href="/pages/whitethang.html">White Thang</a></li>
-            <li><a href="/pages/leprechaun.html">Leprechaun</a></li>
-        </ul>
-    </li>
-    <li><a href="/pages/contact.html">Contact</a></li>
-  </ul>
-</nav>`;
+  connectedCallback() {
+    
+    const prefix = window.location.pathname.includes('/pages/') ? '../' : './';
+
+    this.innerHTML = `
+    <nav>
+      <ul>
+        <li><a href="${prefix}index.html">HOME</a></li>
+        <li><a href="${prefix}pages/cryptidfaq.html">What's A Cryptid?</a></li>
+        
+        <li class="dropdown">
+          <a href="${prefix}pages/famouscryptids.html">Famous Cryptids ▾</a>
+          <ul class="dropdown-menu">
+            <li><a href="${prefix}pages/ufo.html">UFO</a></li>
+            <li><a href="${prefix}pages/whitethang.html">White Thang</a></li>
+            <li><a href="${prefix}pages/leprechaun.html">Leprechaun</a></li>
+          </ul>
+        </li>
+        
+        <li><a href="${prefix}pages/contact.html">Contact</a></li>
+      </ul>
+    </nav>`;
   }
-  }
+}
 
 customElements.define("navbar-component", Navbar);
