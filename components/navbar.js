7811e6ac66fb2f576a680d6ccffd321d45175b2c
wxmansam <wxmansam@gmail.com>
2026-04-26 21:08:27 -0500

    Update navbar links to relative paths

--- components/navbar.js
+++ components/navbar.js
@@ -4,32 +4,24 @@ class Navbar extends HTMLElement {
   }
 
   
-  connectedCallback() {
-    this.innerHTML = `
-   
-    <header class="main-banner">
-        <div class="banner-content">
-            <h1 class="cryptid-title">ALABAMA CRYPTIDS</h1>
-            <p class="cryptid-subtitle">Uncovering the Unseen in the Heart of Dixie</p>
-        </div>
-    </header>
-    
+connectedCallback() {
+  this.innerHTML = `
     <nav>
       <ul>
-        <li><a href="/cryptidbama/index.html">HOME</a></li>
-        <li><a href="/cryptidbama/pages/cryptidfaq.html">What's A Cryptid?</a></li>
+        <li><a href="../index.html">HOME</a></li>
+        <li><a href="./cryptidfaq.html">What's A Cryptid?</a></li>
         <li class="dropdown">
-          <a href="/cryptidbama/pages/famouscryptids.html">Famous Cryptids ▾</a>
+          <a href="./famouscryptids.html">Famous Cryptids ▾</a>
           <ul class="dropdown-menu">
-            <li><a href="/cryptidbama/pages/ufo.html">UFO</a></li>
-            <li><a href="/cryptidbama/pages/whitethang.html">White Thang</a></li>
-            <li><a href="/cryptidbama/pages/leprechaun.html">Leprechaun</a></li>
+            <li><a href="./ufo.html">UFO</a></li>
+            <li><a href="./whitethang.html">White Thang</a></li>
+            <li><a href="./leprechaun.html">Leprechaun</a></li>
           </ul>
         </li>
-        <li><a href="/cryptidbama/pages/contact.html">Contact</a></li>
+        <li><a href="./contact.html">Contact</a></li>
       </ul>
     </nav>`;
-  }
 }
+  }
 
 customElements.define("navbar-component", Navbar);
